ee6737da5633b3d07c9df39cbc2fc30ff35181e3
wasantos <william.alves@gmail.com>
2022-01-13 22:25:56 -0300

    Fix instance_group

--- modules/emr/main.tf
+++ modules/emr/main.tf
@@ -15,7 +15,7 @@ resource "aws_emr_cluster" "emr-spark-cluster" {
 
   ebs_root_volume_size = "12"
 
-  aws_emr_instance_group {
+  aws_emr_instance_group "task" {
     name           = "EMR master"
     instance_role  = "MASTER"
     instance_type  = "${var.master_instance_type}"
@@ -28,7 +28,7 @@ resource "aws_emr_cluster" "emr-spark-cluster" {
     }
   }
 
-  aws_emr_instance_group {
+  aws_emr_instance_group "task" {
     name           = "EMR slave"
     instance_role  = "CORE"
     instance_type  = "${var.core_instance_type}"
